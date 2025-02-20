fc604597373dbde1bf4b7fc9e114443a267f658f
ianjoshi <55448380+ianjoshi@users.noreply.github.com>
2025-02-20 14:13:25 +0100

    Fix energibridge measurements

--- energibridge_executor.py
+++ energibridge_executor.py
@@ -1,11 +1,12 @@
+import os
 import subprocess
-import time
+from dotenv import load_dotenv
 
 class EnergibridgeExecutor:
     """
     A class to handle the execution of EnergiBridge commands for energy measurements.
     """
-    def __init__(self, output_file="results/results.csv", max_measurement_duration=10):
+    def __init__(self, max_measurement_duration=10):
         """
         Initializes the EnergiBridgeExecutor with output file and duration.
         
@@ -13,11 +14,12 @@ class EnergibridgeExecutor:
         - output_file (str): Path to store the measurement results.
         - max_measurement_duration (int): Maximum duration in seconds for measurement.
         """
-        self.output_file = output_file
         self.max_measurement_duration = max_measurement_duration
         self.rapl_service = "rapl"
-        self.driver_path = "energibridge/LibreHardwareMonitor.sys"
-        self.energibridge_exe = "energibridge/energibridge"
+        self.energibridge_exe = ".\\energibridge\\energibridge"
+
+        load_dotenv()
+        self.driver_path = os.getenv("ENERGIBRIDGE_DRIVER_PATH")
     
     def _run_command(self, command):
         """
@@ -30,8 +32,8 @@ class EnergibridgeExecutor:
         - str: Output of the command execution.
         """
         try:
-            command = f"powershell -Command \"Start-Process cmd -ArgumentList '/c {command}' -Verb RunAs\""
             result = subprocess.run(command, shell=True, check=True, capture_output=True, text=True)
+            print(result.stdout.strip())
             print(result.stderr.strip())
             return result.stdout.strip()
         except subprocess.CalledProcessError as e:
@@ -45,7 +47,6 @@ class EnergibridgeExecutor:
         print("Starting EnergiBridge service...")
         self._run_command(f'sc.exe create {self.rapl_service} type=kernel binPath="{self.driver_path}"')
         self._run_command(f'sc.exe start {self.rapl_service}')
-        time.sleep(5)  # Wait for service to start
 
     def stop_service(self):
         """
@@ -55,10 +56,10 @@ class EnergibridgeExecutor:
         self._run_command(f'sc.exe stop {self.rapl_service}')
         self._run_command(f'sc.exe delete {self.rapl_service}')
 
-    def run_measurement(self):
+    def run_measurement(self, output_file="results/results.csv"):
         """
         Runs EnergiBridge measurement and stores the results in the specified file.
         """
         print(f"Running measurement...")
-        self._run_command(f'{self.energibridge_exe} -o {self.output_file} --summary timeout {self.max_measurement_duration}')
+        self._run_command(f'{self.energibridge_exe} -o {output_file} --summary timeout {self.max_measurement_duration}')
         print("Measurement complete.")
